d711ef6771e114ca474a03ac845a0205464553df
Jeremy <jmshasha@gmail.com>
2017-07-17 15:26:17 -0700

    my first commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var WorldLabel: UILabel!
+    
+    var buttonCount = 0
+    
+    @IBAction func pushMeButtonPressed(_ sender: Any) {
+        
+        WorldLabel.text = "sup partner!"
+        buttonCount += 1
+        print (buttonCount)
+        if buttonCount >= 10 {
+            WorldLabel.text = "you pushed the button too many times"
+        }
+    }
+    
+    
+    @IBAction func newButtonPressed(_ sender: Any) {
+        
+        WorldLabel.text = "Buttons are cool"
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
     override func didReceiveMemoryWarning() {
